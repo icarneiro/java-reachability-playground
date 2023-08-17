b1120cb46f2a1bc144509562c4ee7675be80eac4
Igor Carneiro <igor.ribeiro.carneiro@gmail.com>
2023-08-17 13:18:44 -0300

    Update sqli.java

--- src/main/java/sqli.java
+++ src/main/java/sqli.java
@@ -1,4 +1,4 @@
-public class Unzipper {
+public class SQLi {
   String username = request.getParameter("username");
   Statement statement = connection.createStatement();  
   String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AND NOT role = 'admin')";
